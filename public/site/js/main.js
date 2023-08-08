03dbb56e8affa929530d8d9c279b273d454beb59
PhuNguyeen <ducphu3856@gmail.com>
2023-08-08 09:07:33 +0700

    add

---
+++ public/site/js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $("#btn-contact").click(function(){
+        sendContact();
+    });
+    
+    if($(window).width() < 992) {
+        $(".dropdown > a").click(function(e){
+            e.preventDefault();
+            $(".dropdown-content").slideToggle();
+
+        });
+    }
+    
+});
+
+function sendContact() {
+    var name = $("#contact input[type=text][name=name]");
+    var phone = $("#contact input[type=number][name=phone]");
+    var address = $("#contact input[type=text][name=address]");
+    var content = $("#contact textarea");
+    if(name.val() !== '' && phone.val() !== '' && address.val() !== '' && content.val() !== '') {
+        $.ajax({	    	
+            url: base_url + 'contact/ajax_contact',
+            type: 'POST',
+            data: {name: name.val(), phone: phone.val(), address: address.val(), content: content.val()},
+            dataType: 'JSON',
+            success: function (data) {
+                if(data.status === 1) {             
+                    name.val('');
+                    phone.val('');
+                    address.val('');
+                    content.val('');
+                    alert('Yêu cầu đã được gửi. Rất cảm ơn quý khách!');
+                }else {
+                    alert('Lỗi hệ thống');
+                }
+            }
+        });
+    }else {
+        alert('Vui lòng nhập đầy đủ thông tin!');
+    }
+
+}
